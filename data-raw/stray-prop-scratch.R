90a538fda012a90f8d0b38f541f9d38f9d466786
emanuel <erodriguez@flowwest.com>
2021-01-28 13:00:53 -0800

    comments for easier debug and adds scratch work for checking data conversion progress

---
+++ data-raw/stray-prop-scratch.R
@@ -0,0 +1,16 @@
+stray_props_by_month <- sapply(3:6, function(month) {adult_stray(wild = 1,
+            natal_flow = prop_flow_natal[ , year],
+            south_delta_watershed = south_delta_routed_watersheds,
+            cross_channel_gates_closed = cc_gates_days_closed[month])}
+)
+
+stray_by_month <- sapply(1:4, function(month) {
+  rbinom(n = 31, adults, stray_props_by_month[, month])
+})
+south_delta_routed_watersheds
+# strays CC
+as.vector(rmultinom(1,round(sum(stray_by_month[, 2]*south_delta_routed_watersheds)),cross_channel_stray_rate))
+
+# strays OTH
+as.vector(rmultinom(1,round(sum(stray_by_month[, 1]*(1-south_delta_routed_watersheds))), stray_rate))
+
