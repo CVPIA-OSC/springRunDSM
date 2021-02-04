0675a548bf67159923454237a7d3daba7d359ea8
emanuel <erodriguez@flowwest.com>
2021-02-04 11:56:09 -0800

    updates simulation seeding years

--- R/get_spawning_adults.R
+++ R/get_spawning_adults.R
@@ -11,7 +11,7 @@ get_spawning_adults <- function(year, adults, hatch_adults) {
     rmultinom(1, adult_seeds[watershed], month_return_proportions)
   }))
   
-  if (year < 5) {
+  if (year <= 5) {
     natural_adults_by_month <- sapply(1:4, function(month) {
       rbinom(n = 31, 
              size = round(adults_by_month[, month]), 
