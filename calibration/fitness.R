7aa39dbfa34315662712f1e222fb5090c585bec4
emanuel <erodriguez@flowwest.com>
2021-08-19 12:41:29 -0700

    updates watershed used in calibration

--- calibration/fitness.R
+++ calibration/fitness.R
@@ -1,27 +1,19 @@
-# Fitness Function ------------------
-fall_run_fitness <- function(
+spring_run_fitness <- function(
   known_adults,
   seeds,
   params,
-  surv_adult_enroute,
-  surv_adult_prespawn,
-  surv_egg_to_fry,
-  bypass_surv_juv,
-  upsac_surv_juv,
-  butte_surv_juv,
-  clear_surv_juv,
-  deer_surv_juv,
-  mill_surv_juv,
-  sac_surv_juv,
-  feather_and_bear_surv_juv,
-  yuba_surv_juv,
-  american_surv_juv,
-  deltatribs_surv_juv,
-  moke_surv_juv,
-  merced_surv_juv,
-  stan_surv_juv,
-  tuol_surv_juv,
-  sj_surv_juv,
+  surv_adult_enroute_int,
+  surv_adult_prespawn_int,
+  surv_egg_to_fry_int,
+  default_surv_rear_int,
+  battle_clear_surv_rear_int,
+  butte_surv_rear_int,
+  deer_surv_rear_int,
+  mill_surv_rear_int,
+  sac_surv_rear_int,
+  feather_surv_rear_int,
+  yuba_surv_rear_int,
+  sj_surv_rear_int,
   surv_juv_rear_contact_points,
   surv_juv_rear_prop_diversions,
   surv_juv_rear_total_diversions,
@@ -30,56 +22,54 @@ fall_run_fitness <- function(
   surv_juv_delta_contact_points,
   surv_juv_delta_total_diverted,
   surv_juv_outmigration_sj_int,
-  default_ocean_entry_surv,
-  upsac_ocean_entry_surv,
-  butte_ocean_entry_surv,
-  deer_ocean_entry_surv,
-  mill_ocean_entry_surv,
-  midsactribs_ocean_entry_surv,
-  yuba_ocean_entry_surv,
-  american_ocean_entry_surv,
-  deltatribs_ocean_entry_surv,
-  moke_ocean_entry_surv,
-  merced_ocean_entry_surv,
-  stan_ocean_entry_surv,
-  tuol_ocean_entry_surv) {
-
+  surv_juv_outmigration_sac_delta_intercept_one,
+  surv_juv_outmigration_sac_delta_intercept_two,
+  surv_juv_outmigration_sac_delta_intercept_three,
+  default_ocean_int,
+  butte_ocean_int,
+  deer_battle_clear_ocean_int,
+  mill_ocean_int,
+  bear_feather_ocean_int,
+  yuba_ocean_int
+) {
   params_init <- params
-
-  params_init$..surv_adult_enroute_int = surv_adult_enroute
-  params_init$..surv_adult_prespawn_int = surv_adult_prespawn
-  params_init$..surv_egg_to_fry_int = surv_egg_to_fry
-  params_init$..surv_juv_rear_int = c(`Upper Sacramento River` = upsac_surv_juv,
-                                      `Antelope Creek` = deer_surv_juv,
-                                      `Battle Creek` = deer_surv_juv,
-                                      `Bear Creek` = deer_surv_juv,
-                                      `Big Chico Creek` = deer_surv_juv,
-                                      `Butte Creek` = butte_surv_juv,
-                                      `Clear Creek` = clear_surv_juv,
-                                      `Cottonwood Creek` = deer_surv_juv,
-                                      `Cow Creek` = deer_surv_juv,
-                                      `Deer Creek` = deer_surv_juv,
-                                      `Elder Creek` = deer_surv_juv,
-                                      `Mill Creek` = mill_surv_juv,
-                                      `Paynes Creek` = deer_surv_juv,
-                                      `Stony Creek` = deer_surv_juv,
-                                      `Thomes Creek` = deer_surv_juv,
-                                      `Upper-mid Sacramento River` = sac_surv_juv,
-                                      `Sutter Bypass` = bypass_surv_juv,
-                                      `Bear River` = feather_and_bear_surv_juv,
-                                      `Feather River` = feather_and_bear_surv_juv,
-                                      `Yuba River` = yuba_surv_juv,
-                                      `Lower-mid Sacramento River` = sac_surv_juv,
-                                      `Yolo Bypass` = bypass_surv_juv,
-                                      `American River` = american_surv_juv,
-                                      `Lower Sacramento River` = sac_surv_juv,
-                                      `Calaveras River` = deltatribs_surv_juv,
-                                      `Cosumnes River` = deltatribs_surv_juv,
-                                      `Mokelumne River` = moke_surv_juv,
-                                      `Merced River` = merced_surv_juv,
-                                      `Stanislaus River` = stan_surv_juv,
-                                      `Tuolumne River` = tuol_surv_juv,
-                                      `San Joaquin River` = sj_surv_juv)
+  
+  # Juvenile rearing survival coefficients and variables
+  params_init$..surv_juv_rear_int = c(`Upper Sacramento River` = default_surv_rear_int, 
+                                      `Antelope Creek` = default_surv_rear_int, 
+                                      `Battle Creek` = battle_clear_surv_rear_int,
+                                      `Bear Creek` = default_surv_rear_int, 
+                                      `Big Chico Creek` = default_surv_rear_int, 
+                                      `Butte Creek` = butte_surv_rear_int,
+                                      `Clear Creek` = 	battle_clear_surv_rear_int, 
+                                      `Cottonwood Creek` = default_surv_rear_int, 
+                                      `Cow Creek` = default_surv_rear_int,
+                                      `Deer Creek` = deer_surv_rear_int, 
+                                      `Elder Creek` = default_surv_rear_int, 
+                                      `Mill Creek` = mill_surv_rear_int,
+                                      `Paynes Creek` = default_surv_rear_int, 
+                                      `Stony Creek` = default_surv_rear_int, 
+                                      `Thomes Creek` = default_surv_rear_int,
+                                      `Upper-mid Sacramento River` = sac_surv_rear_int, 
+                                      `Sutter Bypass` = default_surv_rear_int,
+                                      `Bear River` = default_surv_rear_int, 
+                                      `Feather River` = feather_surv_rear_int, 
+                                      `Yuba River` = yuba_surv_rear_int,
+                                      `Lower-mid Sacramento River` = 	sac_surv_rear_int, 
+                                      `Yolo Bypass` = default_surv_rear_int, 
+                                      `American River` = default_surv_rear_int,
+                                      `Lower Sacramento River` = sac_surv_rear_int, 
+                                      `Calaveras River` = default_surv_rear_int, 
+                                      `Cosumnes River` = default_surv_rear_int,
+                                      `Mokelumne River` = default_surv_rear_int, 
+                                      `Merced River` = default_surv_rear_int, 
+                                      `Stanislaus River` = default_surv_rear_int,
+                                      `Tuolumne River` = default_surv_rear_int, 
+                                      `San Joaquin River` = sj_surv_rear_int)
+  
+  params_init$..surv_adult_enroute_int = surv_adult_enroute_int
+  params_init$..surv_adult_prespawn_int = surv_adult_prespawn_int # they hard coded from fall run
+  params_init$..surv_egg_to_fry_int = surv_egg_to_fry_int  # they hard coded from fall run
   params_init$..surv_juv_rear_contact_points = surv_juv_rear_contact_points
   params_init$..surv_juv_rear_prop_diversions = surv_juv_rear_prop_diversions
   params_init$..surv_juv_rear_total_diversions = surv_juv_rear_total_diversions
@@ -88,60 +78,66 @@ fall_run_fitness <- function(
   params_init$..surv_juv_delta_contact_points = surv_juv_delta_contact_points
   params_init$..surv_juv_delta_total_diverted = surv_juv_delta_total_diverted
   params_init$..surv_juv_outmigration_sj_int = surv_juv_outmigration_sj_int
+  
+  params_init$..surv_juv_outmigration_sac_delta_intercept_one = surv_juv_outmigration_sac_delta_intercept_one
+  params_init$..surv_juv_outmigration_sac_delta_intercept_two = surv_juv_outmigration_sac_delta_intercept_two
+  params_init$..surv_juv_outmigration_sac_delta_intercept_three = surv_juv_outmigration_sac_delta_intercept_three
+  
+  # Ocean entry success coefficient and variable
   params_init$..ocean_entry_success_int = c(
-    `Upper Sacramento River` = upsac_ocean_entry_surv,
-    `Antelope Creek` = default_ocean_entry_surv,
-    `Battle Creek` = default_ocean_entry_surv,
-    `Bear Creek` = default_ocean_entry_surv,
-    `Big Chico Creek` = default_ocean_entry_surv,
-    `Butte Creek` = butte_ocean_entry_surv,
-    `Clear Creek` = default_ocean_entry_surv,
-    `Cottonwood Creek` = default_ocean_entry_surv,
-    `Cow Creek` = default_ocean_entry_surv,
-    `Deer Creek` = deer_ocean_entry_surv,
-    `Elder Creek` = default_ocean_entry_surv,
-    `Mill Creek` = mill_ocean_entry_surv,
-    `Paynes Creek` = default_ocean_entry_surv,
-    `Stony Creek` = default_ocean_entry_surv,
-    `Thomes Creek` = default_ocean_entry_surv,
-    `Upper-mid Sacramento River` = default_ocean_entry_surv,
-    `Sutter Bypass` = default_ocean_entry_surv,
-    `Bear River` = midsactribs_ocean_entry_surv,
-    `Feather River` = midsactribs_ocean_entry_surv,
-    `Yuba River` = yuba_ocean_entry_surv,
-    `Lower-mid Sacramento River` = default_ocean_entry_surv,
-    `Yolo Bypass` = default_ocean_entry_surv,
-    `American River` = american_ocean_entry_surv,
-    `Lower Sacramento River` = default_ocean_entry_surv,
-    `Calaveras River` = deltatribs_ocean_entry_surv,
-    `Cosumnes River` = deltatribs_ocean_entry_surv,
-    `Mokelumne River` = moke_ocean_entry_surv,
-    `Merced River` = merced_ocean_entry_surv,
-    `Stanislaus River` = stan_ocean_entry_surv,
-    `Tuolumne River` = tuol_ocean_entry_surv,
-    `San Joaquin River` = default_ocean_entry_surv)
-
-
-  keep <- c(1,6,7,10,12,19,20,23,26:30)
-  num_obs <- rowSums(!is.na(known_adults[keep, 6:20]))
-  total_obs <- sum(!is.na(known_adults[keep, 6:20]))
+    `Upper Sacramento River` = default_ocean_int,
+    `Antelope Creek` = default_ocean_int,
+    `Battle Creek` = deer_battle_clear_ocean_int,
+    `Bear Creek` = default_ocean_int,
+    `Big Chico Creek` = default_ocean_int,
+    `Butte Creek` = butte_ocean_int,
+    `Clear Creek` = deer_battle_clear_ocean_int,
+    `Cottonwood Creek` = default_ocean_int,
+    `Cow Creek` = default_ocean_int,
+    `Deer Creek` = deer_battle_clear_ocean_int,
+    `Elder Creek` = default_ocean_int,
+    `Mill Creek` = mill_ocean_int,
+    `Paynes Creek` = default_ocean_int,
+    `Stony Creek` = default_ocean_int,
+    `Thomes Creek` = default_ocean_int,
+    `Upper-mid Sacramento River` = default_ocean_int,
+    `Sutter Bypass` = default_ocean_int,
+    `Bear River` = bear_feather_ocean_int,
+    `Feather River` = bear_feather_ocean_int,
+    `Yuba River` = yuba_ocean_int,
+    `Lower-mid Sacramento River` = default_ocean_int,
+    `Yolo Bypass` = default_ocean_int,
+    `American River` = default_ocean_int,
+    `Lower Sacramento River` = default_ocean_int,
+    `Calaveras River` = default_ocean_int,
+    `Cosumnes River` = default_ocean_int,
+    `Mokelumne River` = default_ocean_int,
+    `Merced River` = default_ocean_int,
+    `Stanislaus River` = default_ocean_int,
+    `Tuolumne River` = default_ocean_int,
+    `San Joaquin River` = default_ocean_int
+  )
+  
+  keep <- c(3L, 6L, 10L, 12L, 19L, 20L)
+  num_obs <- rowSums(!is.na(known_adults[keep, 6:19]))
+  total_obs <- sum(!is.na(known_adults[keep, 6:19]))
   weights <- num_obs / total_obs
-
-
+  
+  
   tryCatch({
-    preds <- fall_run_model(mode = "calibrate",
-                            seeds = seeds,
-                            stochastic = FALSE,
-                            ..params = params_init)
-
-    known_nats <- known_adults[keep, 6:20] * (1 - params_init$proportion_hatchery[keep])
+    preds <- spring_run_model(mode = "calibrate",
+                              seeds = seeds,
+                              stochastic = FALSE,
+                              ..params = params_init)
+    
+    known_nats <- known_adults[keep, 6:19] * (1 - params_init$proportion_hatchery[keep])
     mean_escapent <-rowMeans(known_nats, na.rm = TRUE)
-
+    
     # watershed_cor <- sapply(1:length(keep), function(i) {
     #   cor(preds[i,], known_nats[i,], use = "pairwise.complete.obs")
     # })
     sse <- sum(((preds[keep,] - known_nats)^2 * weights)/mean_escapent, na.rm = TRUE)
-
+    
     return(sse)
   },
   error = function(e) return(1e12),
@@ -149,3 +145,15 @@ fall_run_fitness <- function(
   )
 }
 
+
+
+# x <- runif(29)
+# print(spring_run_fitness(
+#   known_adults = known_adults,
+#   seeds = calibration_seeds,
+#   params = params,
+#   x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10],
+#   x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[18], x[19],
+#   x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28],
+#   x[29]
+# ))
--- calibration/run-calibration.R
+++ calibration/run-calibration.R
@@ -1,4 +1,4 @@
-library(fallRunDSM)
+library(springRunDSM)
 library(GA)
 library(dplyr)
 library(tidyr)
@@ -8,42 +8,50 @@ library(readr)
 source("calibration/fitness.R")
 source("calibration/update-params.R")
 
-params <- DSMCalibrationData::set_synth_years(fallRunDSM::params)
-
+params <- DSMCalibrationData::set_synth_years(springRunDSM::params)
 current_best_solution <- read_rds("calibration/calibration-result.rds")
+
+# proportion of spring run in feather/yuba for year 2010-2012
+spring_prop_feather_yuba <- mean(c(0.076777295, 0.056932196, 0.081441457))
+
+known_adults <- DSMCalibrationData::grandtab_observed$spring
+known_adults["Feather River", ] <- DSMCalibrationData::grandtab_observed$fall["Feather River", ] * spring_prop_feather_yuba
+known_adults["Yuba River", ] <- DSMCalibrationData::grandtab_observed$fall["Yuba River", ] * spring_prop_feather_yuba
+
+calibration_seeds <- DSMCalibrationData::grandtab_imputed$spring
+calibration_seeds["Feather River", ] <- DSMCalibrationData::grandtab_imputed$fall["Feather River", ] * spring_prop_feather_yuba
+calibration_seeds["Yuba River", ] <- DSMCalibrationData::grandtab_imputed$fall["Yuba River", ] * spring_prop_feather_yuba
 
 # Perform calibration --------------------
 res <- ga(type = "real-valued",
           fitness =
-            function(x) -fall_run_fitness(
-              known_adults = DSMCalibrationData::grandtab_observed$fall,
-              seeds = DSMCalibrationData::grandtab_imputed$fall,
+            function(x) -spring_run_fitness(
+              known_adults = known_adults,
+              seeds = calibration_seeds,
               params = params,
               x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10],
               x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[18], x[19],
               x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28],
-              x[29], x[30], x[31], x[32], x[33], x[34], x[35], x[36], x[37],
-              x[38], x[39], x[40]
+              x[29]
             ),
-          lower = c(2.5, rep(-3.5, 39)),
-          upper = rep(3.5, 40),
+          lower = c(2.5, rep(-3.5, 28)),
+          upper = rep(3.5, 29),
           popSize = 150,
           maxiter = 10000,
           run = 50,
           parallel = TRUE,
-          pmutation = .3,
-          suggestions = current_best_solution@solution)
+          pmutation = .3)
 
 readr::write_rds(res, paste0("calibration/fits/result-", Sys.Date(), ".rds"))
 
 # Evaluate Results ------------------------------------
 
-keep <- c(1,6,7,10,12,19,20,23,26:30)
-r1_solution <- current_best_solution@solution
+keep <- c(1L, 2L, 3L, 5L, 6L, 7L, 8L, 10L, 12L, 19L, 20L)
+r1_solution <- res@solution[1, ]
 
-r1_params <- update_params(x = r1_solution, fallRunDSM::params)
+r1_params <- update_params(x = r1_solution, springRunDSM::params)
 r1_params <- DSMCalibrationData::set_synth_years(r1_params)
-r1_sim <- fall_run_model(seeds = DSMCalibrationData::grandtab_imputed$fall, mode = "calibrate",
+r1_sim <- spring_run_model(seeds = calibration_seeds, mode = "calibrate",
                          ..params = r1_params,
                          stochastic = FALSE)
 
@@ -55,7 +63,7 @@ r1_nat_spawners <- as_tibble(r1_sim[keep, ,drop = F]) %>%
          year = readr::parse_number(year) + 5)
 
 
-r1_observed <- as_tibble((1 - fallRunDSM::params$proportion_hatchery[keep]) * DSMCalibrationData::grandtab_observed$fall[keep,, drop=F]) %>%
+r1_observed <- as_tibble((1 - springRunDSM::params$proportion_hatchery[keep]) * known_adults[keep,, drop=F]) %>%
   mutate(watershed = DSMscenario::watershed_labels[keep]) %>%
   gather(year, spawners, -watershed) %>%
   mutate(type = "observed", year = as.numeric(year) - 1997) %>%
@@ -72,14 +80,9 @@ r1_eval_df %>%
 
 r1_eval_df %>%
   spread(type, spawners) %>%
-  # filter(watershed == "Yuba River") %>%
+  filter(watershed == "Yuba River") %>%
   ggplot(aes(observed, simulated)) + geom_point() +
-  geom_abline(intercept = 0, slope = 1) +
-  labs(title = "Observed vs Predicted",
-       x = "Observed Natural Spawners",
-       y = "Predicted Natural Spawners") +
-  xlim(0, 60000) +
-  ylim(0, 30000)
+  geom_abline(intercept = 0, slope = 1)
 
 x <- r1_eval_df %>%
   spread(type, spawners) %>%
